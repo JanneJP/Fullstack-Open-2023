5d928289b7ea354fc0a5f0cf1eb9a637b4bfe13b
Janne Jokinen <Jokinen.Janne.P@gmail.com>
2023-04-03 20:46:17 +0300

    04.21

---
+++ Exercises/04/04.21/blogilista/backend/src/controllers/blogs.js
@@ -0,0 +1,82 @@
+const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+
+  return blog ? response.json(blog) : response.status(404).end()
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+
+  response.status(204).json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = await Blog.findByIdAndRemove(request.params.id)
+
+  if (blog === null) {
+    response.status(404).end()
+  } else if ( blog.user.toString() !== user._id.toString() ) {
+    return response.status(403).json({ error: 'Not the owner' })
+  } else {
+    response.status(204).end()
+  }
+})
+
+module.exports = blogsRouter
