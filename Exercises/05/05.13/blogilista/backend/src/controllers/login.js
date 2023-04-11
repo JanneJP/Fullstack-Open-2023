e179b2f4a9652ce99477652fc3b695b9d89ca796
Janne Jokinen <Jokinen.Janne.P@gmail.com>
2023-04-11 19:21:56 +0300

    Login API refactor

--- Exercises/05/05.13/blogilista/backend/src/controllers/login.js
+++ Exercises/05/05.13/blogilista/backend/src/controllers/login.js
@@ -18,7 +18,7 @@ loginRouter.post('/', async (request, response) => {
     : await bcrypt.compare(password, user.passwordHash)
 
   if (!(user && passwordCorrect)) {
-    return response.status(401).json({ error: 'invalid username or password' })
+    return response.status(401).json({ error: 'Invalid username or password' })
   }
 
   const userForToken = {
--- Exercises/05/05.13/blogilista/backend/tests/integration/login_api.test.js
+++ Exercises/05/05.13/blogilista/backend/tests/integration/login_api.test.js
@@ -1,88 +1,106 @@
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
 const supertest = require('supertest')
 
-const helper = require('../test_helper')
-const User = require('../../src/models/user')
 const app = require('../../src/app')
+const User = require('../../src/models/user')
 
 const api = supertest(app)
+
+const initialUser = {
+  username: 'login_test_user',
+  name: 'login_test_user',
+  password: 'login_test_password'
+}
 
 beforeEach(async () => {
   await User.deleteMany({})
 
-  await helper.createUser(helper.initialUser)
+  const user = new User({
+    username: initialUser.username,
+    name: initialUser.name,
+    passwordHash: await bcrypt.hash(initialUser.password, 10)
+  })
+
+  await user.save()
 })
 
-describe('When there is a valid user in database', () => {
-  test('Login will be successful', async () => {
+describe('POST requests', () => {
+  test('Login with valid credentials will succeed with status code 200', async () => {
     const credentials = {
-      username: helper.initialUser.username,
-      password: helper.initialUser.password,
+      username: initialUser.username,
+      password: initialUser.password
     }
 
-    const response = await api
+    const loginResponse = await api
       .post('/api/login')
       .send(credentials)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
 
-    expect(response.body).toHaveProperty('token')
+    expect(loginResponse.statusCode).toBe(200)
+    expect(loginResponse.type).toBe('application/json')
+    expect(loginResponse.body.username).toBe(initialUser.username)
+    expect(loginResponse.body.name).toBe(initialUser.name)
+    expect(loginResponse.body).toHaveProperty('id')
+    expect(loginResponse.body).toHaveProperty('token')
   })
 
-  test('Login will fail with invalid username', async () => {
-    const credentials = {
-      username: 'root2',
-      password: helper.initialUser.password,
+  test('Login with invalid credentials will fail with status code 401', async () => {
+    let credentials = {
+      username: initialUser.username,
+      password: 'wrongpassword'
     }
 
-    const response = await api
+    let loginResponse = await api
       .post('/api/login')
       .send(credentials)
-      .expect(401)
-      .expect('Content-Type', /application\/json/)
 
-    expect(response.body.error).toContain('invalid username or password')
-  })
+    expect(loginResponse.statusCode).toBe(401)
+    expect(loginResponse.type).toBe('application/json')
+    expect(loginResponse.body.error).toBe('Invalid username or password')
 
-  test('Login will fail with invalid password', async () => {
-    const credentials = {
-      username: helper.initialUser.username,
-      password: 'password2',
+    credentials = {
+      username: 'doesnotexist',
+      password: initialUser.password
     }
 
-    const response = await api
+    loginResponse = await api
       .post('/api/login')
       .send(credentials)
-      .expect(401)
-      .expect('Content-Type', /application\/json/)
 
-    expect(response.body.error).toContain('invalid username or password')
+    expect(loginResponse.statusCode).toBe(401)
+    expect(loginResponse.type).toBe('application/json')
+    expect(loginResponse.body.error).toBe('Invalid username or password')
   })
 
-  test('Login will fail with a missing password', async () => {
-    const credentials = {
-      username: helper.initialUser.username
+  test('Login with missing credential fields will fail with status code 400', async () => {
+    let credentials = {
+      username: initialUser.username,
     }
 
-    const response = await api
+    let loginResponse = await api
       .post('/api/login')
       .send(credentials)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
 
-    expect(response.body.error).toContain('Missing username or password')
-  })
+    expect(loginResponse.statusCode).toBe(400)
+    expect(loginResponse.type).toBe('application/json')
+    expect(loginResponse.body.error).toBe('Missing username or password')
 
-  test('Login will fail with a missing username', async () => {
-    const credentials = {
-      password: helper.initialUser.password
+    credentials = {
+      password: initialUser.password
     }
 
-    const response = await api
+    loginResponse = await api
       .post('/api/login')
       .send(credentials)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
 
-    expect(response.body.error).toContain('Missing username or password')
+    expect(loginResponse.statusCode).toBe(400)
+    expect(loginResponse.type).toBe('application/json')
+    expect(loginResponse.body.error).toBe('Missing username or password')
   })
+})
+
+afterAll(async () => {
+  await User.deleteMany({})
+
+  await mongoose.connection.close()
 })
