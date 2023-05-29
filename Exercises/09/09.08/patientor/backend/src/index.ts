8597572817d897777788c1356914155aac530563
Janne Jokinen <Jokinen.Janne.P@gmail.com>
2023-05-29 14:48:25 +0300

    09.10

--- Exercises/09/09.08/patientor/backend/src/index.ts
+++ Exercises/09/09.08/patientor/backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
@@ -13,6 +15,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
