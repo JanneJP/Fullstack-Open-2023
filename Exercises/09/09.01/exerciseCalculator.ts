24a7491ded7e48115755c9596b5613dee8a802d5
Janne Jokinen <Jokinen.Janne.P@gmail.com>
2023-05-29 04:51:36 +0300

    09.03

--- Exercises/09/09.01/exerciseCalculator.ts
+++ Exercises/09/09.01/exerciseCalculator.ts
@@ -1,3 +1,30 @@
+interface Exercise {
+  days: number[],
+  target: number
+}
+
+const parseArguments2 = (args: string[]): Exercise => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  const target = args[2]
+  const days = args.slice(3, args.length)
+
+  if (isNaN(Number(target))) {
+    throw new Error(`Target ${target} was not a number`);
+  }
+
+  for(const day in days) {
+    if (isNaN(Number(day))) {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+
+  return {
+    days: days.map(d => Number(d)),
+    target: Number(target)
+  }
+}
+
 interface Results {
   periodLength: number,
   trainingDays: number,
@@ -16,6 +43,7 @@ interface Rating {
 const rate = (dailyHours: number[], target: number): Rating => {
   const total = sum(dailyHours)
   const average = total / dailyHours.length
+
   if(average > target) {
     return { rating: 3, ratingDescription: 'Target reached' }
   } else if (target / average > 0.5) {
@@ -47,4 +75,14 @@ const calculateExercises = (dailyHours: number[], target: number): Results => {
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const { days, target } = parseArguments2(process.argv);
+  console.log(calculateExercises(days, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
