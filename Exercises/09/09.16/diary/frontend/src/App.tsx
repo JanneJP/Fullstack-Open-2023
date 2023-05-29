5ca6f07ea3fbf585ec2c0a1057d783de53c4a39d
Janne Jokinen <Jokinen.Janne.P@gmail.com>
2023-05-29 19:00:18 +0300

    09.18

--- Exercises/09/09.16/diary/frontend/src/App.tsx
+++ Exercises/09/09.16/diary/frontend/src/App.tsx
@@ -4,9 +4,17 @@ import { DiaryEntries } from "./components/DiaryEntries";
 import { DiaryEntry } from './types';
 import { getAllDiaryEntries } from './services/diaryEntryService';
 import { createDiaryEntry } from './services/diaryEntryService';
+import { Notification } from './components/Notification';
 
 const App = () => {
   const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
+
+  const [date, setDate] = useState('');
+  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('');
+  const [comment, setComment] = useState('');
+
+  const [notification, setNotification] = useState('');
 
   useEffect(() => {
     getAllDiaryEntries().then(data => {
@@ -14,22 +22,35 @@ const App = () => {
     })
   }, [])
 
-  const DiaryEntryForm = (): JSX.Element => {
-    const [date, setDate] = useState('');
-    const [visibility, setVisibility] = useState('');
-    const [weather, setWeather] = useState('');
-    const [comment, setComment] = useState('');
-  
-    const diaryEntryCreation = (event: React.SyntheticEvent) => {
-      event.preventDefault();
-  
-      createDiaryEntry({ date: date, visibility: visibility, weather: weather, comment: comment }).then(data => {
-        console.log(data)
-        setDiaryEntries(diaryEntries.concat(data))
-      })
-    };
-  
-    return (
+  const diaryEntryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    createDiaryEntry({ date: date, visibility: visibility, weather: weather, comment: comment }).then(data => {
+      console.log(data)
+      setDiaryEntries(diaryEntries.concat(data))
+
+      setNotification('New entry added!')
+      setTimeout(() => {
+        setNotification('')
+      }, 5000)
+
+      setDate('')
+      setVisibility('')
+      setWeather('')
+      setComment('')
+    }).catch(error => {
+      console.log(error)
+      setNotification(error.response.data)
+      setTimeout(() => {
+        setNotification('')
+      }, 5000)
+    })
+  };
+
+  return (
+    <div>
+      <h1>Diary entries</h1>
+      <Notification notification={notification} />
       <div>
         <form onSubmit={diaryEntryCreation}>
           date
@@ -55,13 +76,6 @@ const App = () => {
           <button type='submit'>add</button>
         </form>
       </div>
-    );
-  };
-
-  return (
-    <div>
-      <h1>Diary entries</h1>
-      <DiaryEntryForm />
       <DiaryEntries diaryEntries={diaryEntries} />
     </div>
   );
